c6b484dd97eee29bfa2a2a7148bd100f5988b815
Adrian Chicaiza <74798975+AdrianChicaiza@users.noreply.github.com>
2023-08-16 10:27:34 -0500

    funcion saludar

---
+++ test.js
@@ -0,0 +1,10 @@
+saludar = function () {
+  let nombre;
+  let apellido;
+  let saludo;
+
+  nombre = document.getElementById("txtNombre").value;
+  apellido = document.getElementById("txtApellido").value;
+  saludo = generarSaludo(nombre, apellido);
+  console.log(saludo);
+};
